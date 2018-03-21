4484fd14800bc8e86c44330b39991a953e622a9f
Денис Макаров <den.mkr@gmail.com>
2018-03-21 11:20:56 +0300

    add nav with items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  items = [
+    {text: "Дашборд", link: "/home"},
+    {text: "Контрагенты", link: "/parties"},
+    {text: "2Do", link: "/2do"},
+    {text: "Сделки", link: "/deals"},
+    {text: "Офис", link: "/office"},
+    {text: "Дизайн", link: "/design"},
+    {text: "Принт", link: "/print"},
+    {text: "Склад", link: "/warehouse"},
+    {text: "Документы", link: "/documents"},
+    {text: "Деньги", link: "/money"},
+    {text: "Аналитика", link: "/analytics"}
+  ];
 }
